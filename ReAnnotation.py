bfcd128ccc1b7b9fd13270bb432c7d3c9942c98b
unknown <biancco@snu.ac.kr>
2022-08-10 16:23:26 +0900

    Before ReAnnot

--- ReAnnotation.py
+++ ReAnnotation.py
@@ -6,15 +6,15 @@ yaml_name = 'data.yaml'
 # 초기 labels가 'labels_old' 폴더로 이동됨
 # 'labels_old'가 이미 있는 경우 'labels'폴더는 바로 삭제됨
 
-ratio_blankimage = 0.3  # Sustain image without any bounding box randomly (0~1)
-size_threshold = False
-tiny_cutoff = 150
+ratio_blankimage = 1  # Sustain image without any bounding box randomly (0~1)
+size_threshold = True
+tiny_cutoff = 200
 large_cutoff = 300000
 img_size = [640, 360]
 
 #######################################################################
 # 데이터셋이 이제 자동으로 감지되므로 필요없음
-dataset_type = 2
+dataset_type = 5
 '''
 0 = Dobo 도보 aihub
 1 = Chair 휠체어 aihub
@@ -29,10 +29,10 @@ target_class={}  # 통합클래스이름:통합라벨
 to_unified_class={}  # 기존클래스이름:통합클래스이름
 src_class={}  # 기존클래스이름:기존라벨
 mapping={}  # 기존라벨:통합라벨
-cases={}  # 통합라벨:Bbox갯수
+cases={}  # 통합클래스이름:Bbox갯수
 final=[]
 train_val_test=[0,0,0]  # Number of each type of data [train, val, test]
-img_box=[0,0,0,0]   # Number of total [images, bboxes, tiny, large]
+img_box=[0,0,0,0,0]   # Number of total [images, bboxes, tiny, large, blank_ripped]
 
     
 def make_mapping():
@@ -41,22 +41,23 @@ def make_mapping():
         convert = to_unified_class[probe]
         if convert in target_class.keys():
             mapping[src_class[probe]] = target_class[convert]
+            cases[convert] = 0
         else:
             if(convert=="none"):
                 continue
-            print(f"Ignored class: {probe}")
+            print(f"== WARNING == Ignored class: {probe}")
 
 def reannotation():
     global src_dir, final
     
     for type in ['train','test','val']:
-    # for type in ['val']:
         old_path = src_dir+'/'+type+'/labels_old/'
         new_path = src_dir+'/'+type+'/labels/'
         if not os.path.exists(old_path):
             continue
         file_list = os.listdir(old_path)
         # print(len(file_list))
+        img_box[0]+=len(file_list)
         
         for txt in file_list:
             with open(old_path+txt, 'r') as old:
@@ -74,7 +75,11 @@ def reannotation():
                         continue
                     if(size_threshold):
                         area = float(bbox[3])*float(bbox[4])*img_size[0]*img_size[1]
-                        if(area>large_cutoff or area<tiny_cutoff):  # 사이즈 limit
+                        if(area<tiny_cutoff):  # 사이즈 limit
+                            img_box[2]+=1
+                            continue
+                        elif(area>large_cutoff):  # 사이즈 limit
+                            img_box[3]+=1
                             continue
 
                     bbox[0] = str(mapping[int(label)])
@@ -83,24 +88,28 @@ def reannotation():
 
                 if(len(bboxes)==0):  # 라벨이 하나도 없는 경우
                     if(random.random()>=ratio_blankimage):
+                        img_box[4]+=1
                         continue
 
-                img_box[0]+=1
                 img_box[1]+=len(bboxes)
                 for bbox in bboxes:
+                    # print(final[int(bbox[0])])
                     cases[final[int(bbox[0])]]+=1
                     result = ' '.join(s for s in bbox)
                     new.write(result+'\n')
                 # image = txt[:txt.find(".txt")]+'.jpg'
                 # image_num+=1
                 new.close()
+            if(type=="train"):
+                train_val_test[0]+=1
+            elif(type=="val"):
+                train_val_test[1]+=1
+            if(type=="test"):
+                train_val_test[2]+=1
 
 def yaml_writer_ReAnnotation():
     global final
-    acc=1
-    while os.path.exists(src_dir+'/data'+str(acc)+'yaml'):
-        acc+=1
-    with open(src_dir+"/data"+str(acc)+".yaml", 'w') as f:
+    with open(src_dir+"/data_A.yaml", 'w') as f:
         f.write("path: "+src_dir+"\ntrain: train/images\nval: val/images\n")
         f.write("test: test/images\n\nnc: %d\nnames: ["%len(final))
         len_ = len(final)
@@ -110,12 +119,13 @@ def yaml_writer_ReAnnotation():
                 f.write(', ')
             else:
                 f.write(']')
-        f.write("\n# Dataset statistics: \nTotal imgs: %d\nTrain-Val-Test: [%d,%d,%d]\n\n"%(img_box[0],
+        f.write("\n# Dataset statistics: \nTotal imgs: %d\nTrain-Val-Test: [%d,%d,%d]\n"%(img_box[0],
             train_val_test[0],train_val_test[1],train_val_test[2]))
+        f.write("Blank images ignored: %d\n\n"%img_box[4])
         f.write("Too small boxes ignored: %d\nToo large boxes ignored: %d\n\n"%(img_box[2],img_box[3]))
         f.write("Total Bbox: %d\nBbox distribution:\n"%(img_box[1]))
         for i in range(len_):
-            f.write("    %s: %d\n"%(final[i],cases[final[i]]))
+            f.write("    %s: %s\n"%(final[i],cases[final[i]]))
         f.close()
 
 def data_init(data_name):
@@ -132,9 +142,9 @@ def data_init(data_name):
     class_json = json.load(open('class.json','r', encoding='UTF-8'))
     target_class = class_json["Final"]["Label"]
     to_unified_class = class_json[data_name]["Mapping"]  
-    final = class_json["Final"]["Original"]
+    final = list(class_json["Final"]["Label"].keys())
     for name in final:
-        cases[name] = 0
+        cases[name] = 'Invalid'
 
 def main():
     if("wesee" in yaml_path.lower() or "wesee" in yaml_name.lower()):
@@ -155,6 +165,9 @@ def main():
             data_name = "Wesee"
         elif dataset_type==3:
             data_name = "Coco"
+        else:
+            print("Wrong dataset type number")
+            return
     print(f"Selected dataset_type: {data_name}")
     data_init(data_name)
     assigned = False
@@ -182,7 +195,7 @@ def main():
 
     make_mapping()
     reannotation()
-    # yaml_writer_ReAnnotation()
+    yaml_writer_ReAnnotation()
 
 if __name__ == "__main__":
     main()
